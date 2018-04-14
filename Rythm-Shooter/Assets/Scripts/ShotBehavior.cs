8651c4fe87c5fdcf891575cf5ee46d8f140d26bd
Sweisian <rswei888@gmail.com>
2018-04-14 18:32:29 -0500

    fixed lockfile issue

---
+++ Rythm-Shooter/Assets/Scripts/ShotBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBehavior : MonoBehaviour {
+
+
+    public GameObject Creator;
+    public float shotspeed = 0.5f;
+
+    public void Init(GameObject creator, Vector2 initialPosition, Vector2 Right)
+    {
+        Creator = creator;
+        transform.position = initialPosition;
+        transform.right = Right;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.right * shotspeed; 
+	}
+}
