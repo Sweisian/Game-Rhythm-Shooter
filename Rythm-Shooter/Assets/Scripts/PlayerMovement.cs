1f96dfd607c69d9c8614104233a092ca7fd1d618
emilyblackman <emilyblackman2019@u.northwestern.edu>
2018-04-14 20:33:17 -0500

    basic movement implemented

--- Rythm-Shooter/Assets/Scripts/PlayerMovement.cs
+++ Rythm-Shooter/Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,12 @@ public class PlayerMovement : MonoBehaviour {
     public bool canmoveinair = true;
     Transform myTransform, tagGround;
     Rigidbody2D mybody;
-    bool isground = true;
+    bool isgrounded = true;
     private Vector2 pos;
-
+    Vector2 moveVel;
+    public float scalingFactor = 0.36f;
+    public float minSpeed = 9f;
+    public float maxSpeed = 5;
 
     // Use this for initialization
     void Start()
@@ -27,33 +30,82 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        isground = Physics2D.Linecast (myTransform.position, tagGround.position);
         Move(Input.GetAxisRaw("L_XAxis_1"));
+        //Respawn();
+        //CheckDeath();
         if (Input.GetButtonDown("A_1"))
         {
             Jump();
         }
-
-        //if (transform.position.y < threshold)
-            //Respawn();
     }
 
 
     public void Move(float horzontalInput)
     {
-        if (!canmoveinair && !isground)
+        if (!canmoveinair && !isgrounded)
             return;
-        Vector2 moveVel = mybody.velocity;
-        moveVel.x = horzontalInput * speed;
-        mybody.velocity = moveVel;
 
+        moveVel = mybody.velocity;
+
+        if (Input.GetAxisRaw("L_XAxis_1") == 0)
+        {
+            // when movement keys are not currently pressed
+            if (isgrounded && Mathf.Abs(mybody.velocity.x) < 1)
+            {
+                // if circle is grounded and almost not moving
+                // this prevents the circle from drifting when the player isn't moving
+                moveVel.x = 0f;
+            }
+            if (!isgrounded)
+            {
+                // if in the air
+                if (Mathf.Abs(moveVel.x) <= minSpeed)
+                {
+                    return; //don't slow down anymore
+                }
+            }
+            if (moveVel.x > 0) //moving to the right
+                moveVel.x -= speed * scalingFactor;
+            else if (moveVel.x < 0) //moving to the left
+                moveVel.x += speed * scalingFactor;
+            mybody.velocity = moveVel; //set the circle's velocity
+        }
+        // else movement keys are pressed	
+        else if (Mathf.Abs(moveVel.x) < maxSpeed)
+        {
+            moveVel.x += horzontalInput * speed;
+            mybody.velocity = moveVel;
+        }
+        else
+        {
+            // don't increase speed if maxSpeed has been reached
+            moveVel.x = horzontalInput * maxSpeed;
+            mybody.velocity = moveVel;
+        }
     }
+
 
     public void Jump()
     {
-        if (isground)
+        if (isgrounded)
             mybody.velocity += jumpvelocity * Vector2.up;
+    }
 
+    // Detect continous collision with the ground
+    void OnCollisionStay2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag == "Ground" || hit.gameObject.tag == "boxToPush")
+        {
+            isgrounded = true;
+        }
+    }
+
+    // Detect collision exit with ground
+    void OnCollisionExit2D(Collision2D exit)
+    {
+        if (exit.gameObject.tag == "Ground" || exit.gameObject.tag == "boxToPush")
+        {
+            isgrounded = false;
+        }
     }
 }
