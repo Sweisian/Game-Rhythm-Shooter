16332ecc58138e1f471ecad0a4c09fdbe8f059f6
Sweisian <rswei888@gmail.com>
2018-04-16 13:25:55 -0500

    attempting to fix weird merge stuff

---
+++ Rythm-Shooter/Assets/Scripts/Script_Trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Trigger : MonoBehaviour
+{
+    private bool isActive = false;
+
+    private GameObject beat;
+
+    public KeyCode myKey;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(myKey) && isActive)
+        {
+            beat.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        isActive = true;
+        beat = c.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        isActive = false;
+    }
+}
