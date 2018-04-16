669bf31c5a8efd2460b00abfed5feb190fda4424
Ryan "Walker" Chilton <rnchltn@gmail.com>
2018-04-16 17:26:53 -0500

    Target
    A target that moves

---
+++ Rythm-Shooter/Assets/Script_Target.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.position = new Vector2(Random.Range(-5, 5), Random.Range(1, 4));
+    }
+}
