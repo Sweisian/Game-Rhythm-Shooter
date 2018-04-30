af087e9daac4a3fe1eba0a3c28fa3ab75a740856
emilyblackman <emilyblackman2019@u.northwestern.edu>
2018-04-30 17:18:55 -0500

    can't spam actions anymore

--- Rythm-Shooter/Assets/Scripts/Character_Behavior.cs
+++ Rythm-Shooter/Assets/Scripts/Character_Behavior.cs
@@ -60,13 +60,14 @@ public class Character_Behavior : MonoBehaviour
     {
 
         InputDevice player = InputManager.ActiveDevice;
-
+        /*
         //temp code to test the trigger functionality
         if (localIsActive && Input.GetKeyDown(KeyCode.F))
         {
             Fire();
             myTrigger.BeatHit(localIsActive);
         }
+        */
 
         //if (Input.GetAxisRaw("TriggersR_1") < -0.1 && Time.time > shotready)
         //    Fire();
@@ -75,7 +76,7 @@ public class Character_Behavior : MonoBehaviour
 /*
         if (Input.GetButtonDown("A_1") && localIsActive)
 */
-        if (player.Action1)
+        if (player.Action1.WasPressed)
         {
             Debug.Log("A Pressed");
             if (myTrigger.GetIsActive())
@@ -107,12 +108,15 @@ public class Character_Behavior : MonoBehaviour
         FallingPhysics();
 
         //Jump Ability
-        if (player.Action2)
+        
+        if (player.Action2.WasPressed)
         {
             Debug.Log("B Pressed");
 
             //Now checks if the trigger is active
-            if (localIsActive && myTrigger.GetIsActive())
+            //if (localIsActive && myTrigger.GetIsActive())
+            if (myTrigger.GetIsActive())
+
             {
                 Jump();
                 myTrigger.BeatHit(localIsActive);
@@ -135,10 +139,11 @@ public class Character_Behavior : MonoBehaviour
 
         
         //Dash Ability
-        if (player.Action3)
+        if (player.Action3.WasPressed)
         {
             Debug.Log("X pressed");
-            if (localIsActive)
+            if (myTrigger.GetIsActive())
+            //if (localIsActive)
             {
                 //negative on the y to invert stick for some reason
                 Dash(movecontrol.Value, -aimcontrol);
