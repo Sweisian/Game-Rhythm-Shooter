63fb1c9ed818a39c74a213e49724311abc0b7f90
Sweisian <rswei888@gmail.com>
2018-05-05 15:55:38 -0500

    minor changes to my Swei scene

--- Rythm-Shooter/Assets/InControl/Source/VersionInfo.cs
+++ Rythm-Shooter/Assets/InControl/Source/VersionInfo.cs
@@ -30,7 +30,7 @@ namespace InControl
                 Patch = 4,
 
                 //Not sure what the issue was. Got a merge conflict. and I just picked one
-                Build = 3895
+                Build = 3903
 
 
 			};
