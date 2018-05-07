94be6728ecf0f261cbc76c98f2add42847848a9a
Sweisian <rswei888@gmail.com>
2018-05-06 22:06:09 -0500

    added basic UI

--- Rythm-Shooter/Assets/_Scripts/Script_GameManager.cs
+++ Rythm-Shooter/Assets/_Scripts/Script_GameManager.cs
@@ -3,29 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Script_GameManager : MonoBehaviour
 {
-
     private float respawntime = 0.2f;
 
     public GameObject player1;
     public float score1;
-    public GameObject player1score;
+    public TextMeshProUGUI player1score;
 
     public GameObject player2;
     public float score2;
-    public GameObject player2score;
+    public TextMeshProUGUI player2score;
 
     public Transform playerOneRespawn;
     public Transform playerTwoRespawn;
 
+    public TextMeshProUGUI gameOverText;
+
+    public int scoreCap = 5;
+
     // Use this for initialization
     void Start ()
 	{
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
+
         player1 = GameObject.FindGameObjectWithTag("PlayerOne");
         player2 = GameObject.FindGameObjectWithTag("PlayerTwo");
+
+        player1score = GameObject.Find("Player One Score").GetComponent<TextMeshProUGUI>();
+        player2score = GameObject.Find("Player Two Score").GetComponent<TextMeshProUGUI>();
+        gameOverText = GameObject.Find("Game Over Text").GetComponent<TextMeshProUGUI>();
+
+        gameOverText.text = "";
     }
 
     //temp testing code
@@ -33,7 +44,25 @@ public class Script_GameManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.R))
         {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
             respawn(player1);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            respawn(player2);
+        }
+
+        if (score1 >= scoreCap)
+        {
+            gameOverText.text = "Game Over. Green Wins!";
+        }
+        if (score2 >= scoreCap)
+        {
+            gameOverText.text = "Game Over. Red Wins!";
         }
     }
 	
@@ -43,7 +72,7 @@ public class Script_GameManager : MonoBehaviour
         if (caller.GetComponent<Character_Behavior>() != null)
         {
             score2 += 1;
-            player1score.GetComponent<Text>().text = score1.ToString();
+            player2score.text = "Victories: " + score2.ToString();
 
             player1.transform.position = playerOneRespawn.position;
             player2.transform.position = playerTwoRespawn.position;
@@ -51,7 +80,7 @@ public class Script_GameManager : MonoBehaviour
         if (caller.GetComponent<Character_Behavior2>() != null)
         {
             score1 += 1;
-            player2score.GetComponent<Text>().text = score2.ToString();
+            player1score.text = "Victories: " + score1.ToString();
 
             player1.transform.position = playerOneRespawn.position;
             player2.transform.position = playerTwoRespawn.position;
