778d223cb8a82e898c586105c3dcb7c62215e2aa
Ryan "Walker" Chilton <rnchltn@gmail.com>
2018-05-21 16:12:48 -0500

    Menu Stuff
    Enough for today's build.

---
+++ Rythm-Shooter/Assets/_Scripts/MenuButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+    public GameObject PlayScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Emily 5_20");
+    }
+
+    void Options()
+    {
+        SceneManager.LoadScene("Options_Menu");
+    }
+
+    void OptionReturn()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    void Replay()
+    { }
+    void GoToStart()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
