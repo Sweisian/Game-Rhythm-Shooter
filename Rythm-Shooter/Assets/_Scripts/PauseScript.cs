3806fc0562e81e65b264278e5277ec4043c44234
Sweisian <rswei888@gmail.com>
2018-06-10 16:33:11 -0500

    added pause menu

---
+++ Rythm-Shooter/Assets/_Scripts/PauseScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    //[SerializeField] public Scene startMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu...");
+        isPaused = false;
+        SceneManager.LoadScene("Start Menu"); //get rid of this hardcode
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+}
